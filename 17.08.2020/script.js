9cbd12fd781c3be757bbcf039a7ccb8f4ad79ff8
stanislav-sn <stas.snezhkovoy@gmail.com>
2020-08-21 14:56:59 +0300

    added homework '17.08.2020'

---
+++ 17.08.2020/script.js
@@ -0,0 +1,22 @@
+tusconsole.log('logging info');
+console.info('info');
+console.warn('warning');
+console.error('error');
+
+'use strict';
+// 1) Найти ошибку
+var x = prompt('Введите Х', '1')
+    y = prompt('Введите Y', '1')
+    z = Number(x) + Number(y);
+
+alert( 'Сумма: ' + z);
+// 2) Вывод чисел
+var a = 12;
+var b = a ** 3;
+var c = a + b;
+
+alert(a);
+alert(b);
+alert(c);
+
+
