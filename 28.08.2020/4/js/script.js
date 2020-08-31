25bb37743bb0b894c2906547251388921685489e
stanislav-sn <stas.snezhkovoy@gmail.com>
2020-08-31 18:01:34 +0300

    made fifth task

---
+++ 28.08.2020/4/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+debugger;
+
+let i = +prompt('Введите число', '1');
+let d = +prompt('Введите диапазон', '1');
+
+for (; i <= d; i++) {
+	if (i % 2 === 0) {
+		alert(sumInput(i));
+	}
+}
+
+
---
+++ 28.08.2020/5/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+for (let i = 1;; i++) {
+	    let number = Math.random();
+	    if (number > 0.99) { 
+	        alert(`${number}, ${i}`);
+	        break;
+	    }
+	}
+
