140c722da7c2cf97cfade75fa4cdc15937eeba06
stanislav-sn <stas.snezhkovoy@gmail.com>
2020-09-04 14:02:19 +0300

    made second task

--- 31.08.2020/js/script.js
+++ 31.08.2020/js/script.js
@@ -9,14 +9,35 @@ let person = {
 	"firstname": "Will",
 	"lastname": "Smit",
 	"age": "51",
-	"job": "actor"	
+	"job": "actor"
 };
-	
-let property = prompt('Enter property', ''); 
+
+let property = prompt('Enter property', '');
 	
 if (property in person) {
 		alert(person[property])
 	} else {
-		person[property] = prompt('Enter value of property', '');   
+		person[property] = prompt('Enter value of property', '');
 	}
-console.log(person);	
+console.log(person);
+
+// 2) Сгенерируйте объект, описывающий модель телефона, заполнив все свойства значениями, 
+	// прочитанными из prompt (например: brand, model, resolution, color...), не используя вспомогательные переменные. 
+	// Добавьте этот гаджет персоне, созданной ранее.
+
+var smartPhone = {}
+
+smartPhone.brand = prompt('Enter brand','IPhone, Samsung, Xiaomi');
+smartPhone.displaySize = prompt('Enter display size','4" - 7"');
+smartPhone.displayType = prompt('Enter display type','IPS or Amoled');
+smartPhone.os = prompt('Enter OS','Android or IOS');
+smartPhone.memory = prompt('Enter ','32-128 Gb');
+
+person.smartPhone = smartPhone;
+console.log(smartPhone);
+
+// 3) Запустите бесконечный цикл. В цикле вызывайте prompt, в котором пишется либо delete, либо update. 
+	// В зависимости от указанного действия, удаляйте или добавляйте (редактируйте) свойство в объекте персоны 
+	// (которая уже имеет телефон). Какое именно свойство удалять или добавлять - также читается из prompt. 
+	// Какое значение будет у добавленного свойства - тоже берется из prompt. При нажатии на Отмена при появлении prompt 
+	// редактирование person заканчивается. И обновленные данные распечатываются в консоли.
